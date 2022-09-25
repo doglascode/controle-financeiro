f57e1088a79930d9fcabcef3e874b7f2ffea9b8f
doglascode <iwalisondouglas@gmail.com>
2022-09-25 10:08:24 -0300

    refactor: initial setup

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    font-family: inherit;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    align-items: center;
+    background-color: #ECECEC;
+    color: #000000;
+    font-family: "Montserrat", sans-serif;
+    flex-direction: column;
+    display: flex;
+    min-height: 90vh;
+    justify-content: center;
+    -webkit-tap-highlight-color: transparent;
+  }
+`;
